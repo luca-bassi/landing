7a7261b27d8fab1f89273374e8b20e95997496c8
luca-bassi <luca96bass@gmail.com>
2023-10-19 21:11:38 +0200

    basic layout art

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,3 +1,13 @@
-<div class="min-h-screen flex items-center justify-center">
-  <h1 class="text-4xl text-primary">manem jef</h1>
+<div class="min-h-screen flex flex-col">
+  <div class="py-3 px-4">
+    <div class="flex justify-center items-center gap-2 rounded-[40px]">
+      <a href="/" class="py-2 px-4 rounded-[40px] flex items-center justify-center">/</a>
+      <a href="art" class="py-2 px-4 rounded-[40px] flex items-center justify-center">art</a>
+      <a href="music" class="py-2 px-4 rounded-[40px] flex items-center justify-center">music</a>
+    </div>
+  </div>
+
+  <div class="mt-44 px-2 flex justify-center items-center">
+    <h1 class="text-4xl text-primary">manem jef</h1>
+  </div>
 </div>
