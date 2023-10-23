d6b3e9fe592953a85a0e7456d458ae4985e8f6be
Luca Bassi <luca96bass@gmail.com>
2023-10-23 14:23:06 +0200

    mobile fixes

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -5,12 +5,12 @@
 <div class="mt-20 mb-5 px-8 lg:px-10 flex flex-col justify-center items-center gap-10">
   <h1 class="text-4xl text-primary cursor-pointer" on:click={togglePalette}>music</h1>
 
-  <div class="flex flex-col md:flex-row justify-center items-start gap-8">
+  <div class="flex flex-col md:flex-row justify-center items-center md:items-start gap-8">
     <img src="images/music/leap.jpg" class="rounded-xl aspect-square object-cover object-center max-h-64 dark:brightness-[.8] dark:contrast-[1.2]"/>
     <iframe src="https://open.spotify.com/embed/album/3Feaju7gniJeGnlLkp5Kd5" class="frame-container" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   </div>
 
-  <div class="flex justify-center items-center text-primary">
+  <!-- <div class="flex justify-center items-center text-primary">
     <a href="https://open.spotify.com/album/3Feaju7gniJeGnlLkp5Kd5" target="_blank">listen</a>
-  </div>
+  </div> -->
 </div>
