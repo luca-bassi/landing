638a3c80d5fb2d9f961bf4219383ba4619a9346d
Luca Bassi <luca96bass@gmail.com>
2023-11-08 15:14:42 +0100

    lazyload

--- src/components/Artwork.svelte
+++ src/components/Artwork.svelte
@@ -1,5 +1,6 @@
 <script>
   import { fly } from 'svelte/transition';
+  import { lazyLoad } from '$lib/lazyLoad.js'
 
   export let thumb;
 
@@ -7,7 +8,7 @@
   const cdnurl = "https://ruuk999.sirv.com"; // '';
 </script>
 
-<img src={`${cdnurl}/${thumb}`} on:click={() => (open=true)} class="rounded-xl aspect-square object-cover object-center cursor-pointer dark:brightness-[.8] dark:contrast-[1.2]" loading="lazy"/>
+<img use:lazyLoad={`${cdnurl}/${thumb}`} on:click={() => (open=true)} class="img rounded-xl aspect-square object-cover object-center cursor-pointer dark:brightness-[.8] dark:contrast-[1.2]"/>
 
 {#if open}
   <div class="flex flex-col gap-2 items-center justify-center fixed top-0 left-0 h-screen w-screen bg-black/70 z-10 backdrop-blur-sm" on:click={() => (open=false)} in:fly={{duration:300}}>
@@ -15,3 +16,10 @@
     <p class="text-background dark:text-on-background">Click anywhere to dismiss</p>
   </div>
 {/if}
+
+<style>
+  .img {
+    opacity: 0;
+    transition: all 1s ease;
+  }
+</style>
